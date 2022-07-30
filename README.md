6b723a562e2099c579adeb7bc8082d12cf6017eb
HP <abdulraoufmkamal@gmail.com>
2022-07-30 15:13:08 +0200

    Updated version of the dynamic mavigation bar

--- README.md
+++ README.md
@@ -5,6 +5,14 @@
 
 > This is a html and css responsive webpage that is designed to provide some tips regarding some of the *IGCSE* important subjects
 
+> This **HTML** webpage has a navigation bar built dynamically so that when the a new section is created in the HTML, it is added to the navigation bar.
+
+> When a new section is scrolled to in the webpage, the corresponding li element in the navigation bar is highlighted, and also the class of the section in unordered list is set to *active*
+
+> When an li element is clicked, the corresponding section is scrolled to in the webpage
+
+
+
 ## Usage and Dependicies
 
 > There is nothing much used in this project except for some media queries for the responsivness. Otherwise, basic **HTML** and **CSS** are used in this project
--- js/app.js
+++ js/app.js
@@ -1,161 +1,88 @@
+// Background colors for the sections
+const colors = ['chocolate', 'darkkhaki', 'fuchsia', 'mediumspringgreen', 'slateblue'];
+
+// Buidling the dynamic navBar
 const navBar = document.querySelector('#navBar');
-
 const list = document.createElement('ul');
+const sections = Array.from(document.getElementsByTagName('section'));
 
-const mathematics = document.createElement('li');
-const physics = document.createElement('li');
-const chemistry = document.createElement('li');
-const biology = document.createElement('li');
-const esl = document.createElement('li');
+// Creating an li element and setting the textContent to the subject name
+// Then appending the li element to the ul element
 
-mathematics.textContent = 'Mathematics';
-physics.textContent = 'Physics';
-chemistry.textContent = 'Chemistry';
-biology.textContent = 'Biology';
-esl.textContent = 'ESL';
+for (let section of sections) {
+    const subject = document.createElement('li');
+    subject.textContent = section.dataset.subjectName;
+    list.appendChild(subject);
+}
 
-list.appendChild(mathematics);
-list.appendChild(physics);
-list.appendChild(chemistry);
-list.appendChild(biology);
-list.appendChild(esl);
-
+// Appending the ul element to the navBar div element
 navBar.appendChild(list);
 
 const content = document.querySelector("div#content");
 const title = document.querySelector('div#page-title');
 const posTitle = title.getBoundingClientRect();
 
-const div1 = document.getElementById('mathematics');
-const div2 = document.getElementById('physics');
-const div3 = document.getElementById('chemistry');
-const div4 = document.getElementById('biology');
-const div5 = document.getElementById('esl');
+// Storing all the div elements in an array
+const divs = Array.from(document.querySelectorAll('div.subject'));
+const posDivs = Array();
 
-const posDiv1 = div1.getBoundingClientRect();
-const posDiv2 = div2.getBoundingClientRect();
-const posDiv3 = div3.getBoundingClientRect();
-const posDiv4 = div4.getBoundingClientRect();
-const posDiv5 = div5.getBoundingClientRect();
+for (let i = 0; i < divs.length; ++i) {
 
-content.addEventListener('scroll', function () {
-
-    if (content.scrollTop < posDiv2.top - posTitle.bottom) {
-
-        mathematics.style.color = 'chocolate';
-        physics.style.color = 'white';
-        chemistry.style.color = 'white';
-        biology.style.color = 'white';
-        esl.style.color = 'white';
-
-        div1.className = 'active';
-        div2.className = '';
-        div3.className = '';
-        div4.className = '';
-        div5.className = '';
-
+    // Please note at line 1, there is an array of 5 colors
+    // If there is more than 5 sections
+    // The next section will be colored again starting from index 0
+    // till 5
+    if (i < colors.length) {
+        divs[i].style.backgroundColor = colors[i];
     }
-
-    else if (content.scrollTop < posDiv3.top - posTitle.bottom) {
-
-        mathematics.style.color = 'white';
-        physics.style.color = 'darkkhaki';
-        chemistry.style.color = 'white';
-        biology.style.color = 'white';
-        esl.style.color = 'white';
-
-        div1.className = '';
-        div2.className = 'active';
-        div3.className = '';
-        div4.className = '';
-        div5.className = '';
-
-    }
-
-    else if (content.scrollTop < posDiv4.top - posTitle.bottom) {
-
-        mathematics.style.color = 'white';
-        physics.style.color = 'white';
-        chemistry.style.color = 'fuchsia';
-        biology.style.color = 'white';
-        esl.style.color = 'white';
-
-        div1.className = '';
-        div2.className = '';
-        div3.className = 'active';
-        div4.className = '';
-        div5.className = '';
-
-    }
-
-    else if (content.scrollTop < posDiv5.top - posTitle.bottom) {
-
-        mathematics.style.color = 'white';
-        physics.style.color = 'white';
-        chemistry.style.color = 'white';
-        biology.style.color = 'mediumspringgreen';
-        esl.style.color = 'white';
-
-        div1.className = '';
-        div2.className = '';
-        div3.className = '';
-        div4.className = 'active';
-        div5.className = '';
-
-    }
-
     else {
-
-        mathematics.style.color = 'white';
-        physics.style.color = 'white';
-        chemistry.style.color = 'white';
-        biology.style.color = 'white';
-        esl.style.color = 'slateblue';
-
-        div1.className = '';
-        div2.className = '';
-        div3.className = '';
-        div4.className = '';
-        div5.className = 'active';
-
+        divs[i].style.backgroundColor = colors[i - colors.length];
     }
+}
+// Storing the DOMRect (dimensions) object in an array
+for (let div of divs) {
+    posDivs.push(div.getBoundingClientRect());
+}
+
+const items = Array.from(document.querySelectorAll('ul li'));
+
+// Adding a scroll so that when the scrollBar is scrolled
+// 1. The appropriate section is highlighted
+// 2. The appropriate section class name is set to 'active'
+content.addEventListener('scroll', function (evt) {
+    for (let i = 0; i < posDivs.length; ++i) {
+        if (content.scrollTop < posDivs[i].bottom - posTitle.bottom) {
+            for (let j = 0; j < sections.length; ++j) {
+                if (j == i) {
+                    // If there is more than 5 sections
+                    // The next section will be colored again starting from index 0
+                    // till 5
+                    if (j < colors.length) items[j].style.color = colors[j];
+                    else items[j].style.color = colors[j - colors.length];
+                    sections[j].className = 'active';
+                }
+                else {
+                    items[j].style.color = 'white';
+                    sections[j].className = '';
+                }
+            }
+            break;
+        }
+    }
+    evt.preventDefault();
+
 
 });
 
-const selection = document.querySelectorAll('div#navBar li');
-
-selection[0].addEventListener('click', function () {
-    content.scroll({
-        top: posDiv1.top,
-        behavior: "smooth"
+// Adding a click event listener so that when a li element is clicked
+// The scrollBar is scrolled to the appropriate section
+const selections = document.querySelectorAll('div#navBar li');
+for (let i = 0; i < selections.length; ++i) {
+    selections[i].addEventListener('click', function (evt) {
+        content.scroll({
+            top: posDivs[i].top,
+            behavior: "smooth"
+        });
+        evt.preventDefault();
     });
-});
-
-selection[1].addEventListener('click', function () {
-    content.scroll({
-        top: posDiv2.top,
-        behavior: "smooth"
-    });
-});
-
-selection[2].addEventListener('click', function () {
-    content.scroll({
-        top: posDiv3.top,
-        behavior: "smooth"
-    });
-
-});
-
-selection[3].addEventListener('click', function () {
-    content.scroll({
-        top: posDiv4.top,
-        behavior: "smooth"
-    });
-});
-
-selection[4].addEventListener('click', function () {
-    content.scroll({
-        top: posDiv5.top,
-        behavior: "smooth"
-    });
-});
+}
